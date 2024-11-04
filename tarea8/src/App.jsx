9af28e851d389fa3633017d76daff93ecf7e4e11
Rouzzen <vicente.neira@usm.cl>
2024-11-03 22:31:00 -0300

    avances de la página principal

--- tarea8/src/App.jsx
+++ tarea8/src/App.jsx
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 function App() {
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleButtonClick = (index) => {
+    setActiveButton(index);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <main>
+          <h1></h1>
+          <div className="Image-container">
+            <img src="/Feria.png" className="App-image" alt="Feria" />
+            <p className="App-text">Explora los temas de tu interés:</p>
+          </div>
+          <div className="Button-group">
+            {['Inteligencia Artificial', 'Realidad Virtual y Aumentada', 'Tecnología en la Educación'].map((label, index) => (
+              <button
+                key={index}
+                onClick={() => handleButtonClick(index)}
+                className={`App-button ${activeButton === index ? 'active' : ''}`}
+              >
+                {label}
+              </button>
+            ))}
+          </div>
+          <p className="Auspician-text">Auspician:</p>
+          <div className="Sponsor-images">
+            <img src="/scotia.png" alt="Scotia" />
+            <img src="/ntt.png" alt="ntt"/>
+            <img src="/soft.png" alt="Scotia"  />
+            <img src="/social.png" alt="Social" />
+          </div>
+          <img src="/inf.png" alt="UTFSM" className="UTFSM" />
+          <p></p>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
